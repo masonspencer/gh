b39f24e871cb83b389245673b0f60287086d9408
mooson <cloud9@masonspencer.com>
2016-03-23 00:53:45 +0000

    Init

---
+++ app/scripts/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name workspaceApp
+ * @description
+ * # workspaceApp
+ *
+ * Main module of the application.
+ */
+angular.module('workspaceApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.ref',
+    'firebase.auth'
+  ]);
