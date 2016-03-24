019600707829283a86bd1b3a88e6e75b05179718
mooson <cloud9@masonspencer.com>
2016-03-24 22:01:10 +0000

    Redoing stuff

--- app/scripts/controllers/listItem.js
+++ app/scripts/controllers/listItem.js
@@ -9,26 +9,16 @@
  */
 angular.module('workspaceApp')
   .controller('listItemCtrl', function ($scope, Ref, $firebaseArray, $timeout) {
-    // synchronize a read-only, synchronized array of geerItem, limit to most recent 10
-    $scope.geerItem = $firebaseArray(Ref.child('geerItem').limitToLast(10));
+    $scope.geerItems = $firebaseArray(Ref.child('geerItem'));
 
-    // display any errors
-    $scope.geerItem.$loaded().catch(alert);
+    // provide a method for adding a message
+    $scope.addItem = function(newItem) {
+      if( newItem ) {
+        // push a message to the end of the array
+        $scope.geerItems.$add({text: newItem})
+          // display any errors
+          .catch(alert);
+      }
+    };
 
-    // // provide a method for adding a message
-    // $scope.addMessage = function(newMessage) {
-    //   if( newMessage ) {
-    //     // push a message to the end of the array
-    //     $scope.messages.$add({text: newMessage})
-    //       // display any errors
-    //       .catch(alert);
-    //   }
-    // };
-
-    function alert(msg) {
-      $scope.err = msg;
-      $timeout(function() {
-        $scope.err = null;
-      }, 5000);
-    }
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,36 @@
  * Controller of the workspaceApp
  */
 angular.module('workspaceApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, Ref, $firebaseArray, $timeout) {
+    // synchronize a read-only, synchronized array of geerItem, limit to most recent 10
+    $scope.geerItem = $firebaseArray(Ref.child('geerItem').limitToLast(10));
+    var testt = $firebaseArray(Ref);
+    // display any errors
+    $scope.geerItem.$loaded().catch(alert);
+console.log(testt);
+alert('working');
+
+
+
+for (var j = 0; j < testt.length; j++){
+
+console.log(testt[j]);
+
+}
+    // // provide a method for adding a message
+    // $scope.addMessage = function(newMessage) {
+    //   if( newMessage ) {
+    //     // push a message to the end of the array
+    //     $scope.messages.$add({text: newMessage})
+    //       // display any errors
+    //       .catch(alert);
+    //   }
+    // };
+
+    function alert(msg) {
+      $scope.err = msg;
+      $timeout(function() {
+        $scope.err = null;
+      }, 5000);
+    }
   });
